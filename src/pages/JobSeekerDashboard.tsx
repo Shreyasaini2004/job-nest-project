76b79573d41be4c7243f7111b493ed0a4335442a
Shreya Saini <115012333+Shreyasaini2004@users.noreply.github.com>
2025-06-27 11:43:21 +0530

    Create JobSeekerDashboard.tsx

---
+++ src/pages/JobSeekerDashboard.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import JobSeekerSidebar from "@/components/JobSeekerSidebar";
+import JobSeekerContent from "@/components/JobSeekerContent";
+import JobSeekerHeader from "@/components/JobSeekerHeader";
+
+const JobSeekerDashboard = () => {
+  const [activeSection, setActiveSection] = useState<'update-details' | 'view-openings' | 'manage-applications'>('update-details');
+
+  return (
+    <div className="min-h-screen bg-background">
+      <SidebarProvider>
+        <div className="min-h-screen flex w-full">
+          <JobSeekerSidebar activeSection={activeSection} onSectionChange={setActiveSection} />
+          <div className="flex-1 flex flex-col">
+            <JobSeekerHeader />
+            <JobSeekerContent activeSection={activeSection} />
+          </div>
+        </div>
+      </SidebarProvider>
+    </div>
+  );
+};
+
+export default JobSeekerDashboard;
